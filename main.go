7f7bdd67e20913709ea1720cadf3389bb0e36ed3
Chris Siannas <xrsia@otenet.gr>
2022-01-05 14:59:00 +0200

    Example 1

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
+import "fmt"
 
-// reference types (pointers, slices, maps, functions, channels) 
+// reference types (pointers, slices, maps, functions, channels)
 
 // interface type
 
 func main(){
-	
+	x:=10
+	myFirstPointer := &x
+
+	fmt.Println("x is",x)
+	fmt.Println("My first pointer is",myFirstPointer)
+	fmt.Println("Contents of x are",*myFirstPointer)
+
+	*myFirstPointer= 15
+	fmt.Println("x now is:",x)
+
 
 }
