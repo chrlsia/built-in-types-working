f7fe6a842c3df6724907f5593e6158a887f7de94
Chris Siannas <xrsia@otenet.gr>
2022-01-05 16:52:31 +0200

    Functions start

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
-import "fmt"
+
 
 // reference types (pointers, slices, maps, functions, channels)
 
 // interface type
 
 func main(){
-	intMap:=make(map[string]int)
-
-	intMap["one"]=1
-	intMap["two"]=2
-	intMap["three"]=3
-	intMap["four"]=4
-	intMap["five"]=5
-
-	for key,value:= range intMap{
-		// maps are not sorted even we put sorted
-		// info in them
-		fmt.Println(key,value)
-	}
 	
-
-	// delete a key,e.g "four" 
-	// delete(intMap,"four")
-
-	el,ok:=intMap["four"]
-	if ok{
-		fmt.Println(el, "is in map")
-	} else {
-		fmt.Println(el, "is not in map")
-	}
-
-	// change the value of a given key
-	intMap["two"]=4
 
 }
