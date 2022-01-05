9ef27d00f6be977d47b18cd581d92ea9695b15b7
Chris Siannas <xrsia@otenet.gr>
2022-01-05 14:32:27 +0200

    Basic types

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
-// basic types (numbers, strings, booleans)
 
+import "log"
+
+// basic types (numbers, strings, booleans)
+var myInt int
+var myUint uint
+var myFloat float32
+var myFloat64 float64
 // aggregate types (array, struct)
 
 // reference types (pointers, slices, maps, functions, channels) 
@@ -8,5 +14,17 @@ package main
 // interface type
 
 func main(){
+	myInt = 10
+	myUint = 20
+
+	myFloat = 10.1
+	myFloat64 = 100.1
 	
+	log.Println(myInt,myUint,myFloat,myFloat64)
+
+	myString:= "Trevor"
+	log.Println(myString)
+	myString="Chris"
+	log.Println(myString)
+
 }
