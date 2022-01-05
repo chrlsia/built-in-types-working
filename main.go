c4ddb23656ab5fe600049f00c9f8dd049e143956
Chris Siannas <xrsia@otenet.gr>
2022-01-05 14:15:05 +0200

    Start

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+// basic types (numbers, strings, booleans)
+
+// aggregate types (array, struct)
+
+// reference types (pointers, slices, maps, functions, channels) 
+
+// interface type
+
+func main(){
+	
+}
