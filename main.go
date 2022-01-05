7ffa6c06862cdf1b78fab40c223a917e53a81f5c
Chris Siannas <xrsia@otenet.gr>
2022-01-05 16:55:26 +0200

    Function 1

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-
+import "fmt"
 
 // reference types (pointers, slices, maps, functions, channels)
 
 // interface type
 
 func main(){
-	
+	z:=addTwoNumber(2,4)
+	fmt.Println(z)
 
+}
+
+func addTwoNumber(x, y int) int {
+	return x+y
 }
