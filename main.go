01f77808695910899ca369121b1d54adc610e6f0
Chris Siannas <xrsia@otenet.gr>
2022-01-05 15:10:01 +0200

    Start slices

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
-import "fmt"
-
 // reference types (pointers, slices, maps, functions, channels)
 
 // interface type
 
 func main(){
-	x:=10
-	myFirstPointer := &x
-
-	fmt.Println("x is",x)
-	fmt.Println("My first pointer is",myFirstPointer)
-	fmt.Println("Contents of x are",*myFirstPointer)
-
-	*myFirstPointer= 15
-	fmt.Println("x now is:",x)
-
-	changeValueOfPointer(&x)
-
-	fmt.Println("After function call x is now",x)
-}
-
-func changeValueOfPointer(num *int){
-	*num = 25
+	
 }
